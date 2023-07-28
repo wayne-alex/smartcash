60cfcd16e422bc8118b7573a213752ac0ec3c1f8
Wayne Alex <waynealex9914@gmail.com>
2023-07-28 13:04:15 +0300

    severless function error

--- smartcash/settings.py
+++ smartcash/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure--v9^h$u%kw!%t(!gestej#h=%kjse5lw$gg6(-uh_t*z_$4@5n'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['.vercel.app','localhost','127.0.0.1', '.now.sh']
 
