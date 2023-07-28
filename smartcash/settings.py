5e8a44e9111d5470d70b63da7bf1c3cb72e75d04
Wayne Alex <waynealex9914@gmail.com>
2023-07-28 13:14:03 +0300

    Solving serverless fuction error

--- smartcash/settings.py
+++ smartcash/settings.py
@@ -77,7 +77,14 @@ WSGI_APPLICATION = 'smartcash.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': config('DATABASE_NAME'),
+        'USER': config('DATABASE_USER'),
+        'PASSWORD': config('DATABASE_PASSWORD'),
+        'HOST': config('DATABASE_HOST'),
+        'PORT': config('DATABASE_PORT'),
+    }
 }
 
 
